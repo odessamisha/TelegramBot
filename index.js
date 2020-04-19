fa85bd13a80607bf5630a568b41efc62bbb70010
odessamisha <odessamisha@gmail.com>
2020-04-19 18:51:58 +0300

    commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var bot = new TelegramBot(token,{ polling : true});
 
 bot.onText(/\/start/, function(msg, match){
     var UserId = msg.from.id;
-    bot.sendMessage(UserId, 'Hello from Mishka');
+    bot.sendMessage(UserId, 'Hello from Misa');
 });
 
 bot.onText(/\/getdate/, function(msg, match){
